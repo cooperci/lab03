a35fbf95b1724386e3adc91dd9ae44245e68f8db
cooperci <ceci.cooper.4@gmail.com>
2021-01-25 14:25:46 -0500

    labthree

---
+++ lab03/lab03/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace lab03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a whole number between 1 and 100: ");
+            int userInput = Int32.Parse(Console.ReadLine());
+            if (userInput % 2 == 0)
+            {
+                if (userInput <= 25)
+                {
+                    Console.WriteLine("Even and less than 25");
+                }
+                else
+                {
+                    Console.WriteLine("Even");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+    }
+}
